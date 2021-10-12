bb8917d67541d52fcb7cb826aaf832f2876e959c
Dmytro Chornyi <dkdmcho@coloplast.com>
2021-10-12 23:15:43 +0300

    next step is reverse

--- SinglyLinkedList/singlyLinkedList.js
+++ SinglyLinkedList/singlyLinkedList.js
@@ -49,6 +49,84 @@ class SinglyLinkedList {
         return current;
     }
 
+    shift() {
+        if(!this.head) return undefined;
+
+        let currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        return currentHead;
+    }
+
+    unshift(val) {
+        let newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+
+        this.length++;
+        return this;
+    }
+
+    get(index) {
+        if(index < 0 || index >= this.length) return null;
+
+        let counter = 0;
+        let current = this.head;
+
+        while(counter !== index) {
+            current = current.next;
+            counter++;
+        }
+
+        return current;
+    }
+
+    set(index, val) {
+        let foundNode = this.get(index);
+
+        if(foundNode) {
+            foundNode.val = val;
+            return true;
+        }
+
+        return false;
+    }
+
+    insert(index, val) {
+        if(index < 0 || index > this.length) return false;
+
+        if(index === this.length) return this.push(val);
+        if(index === 0) return this.unshift(val);
+
+        let newNode = new Node(val);
+        let prev = this.get(index - 1);
+        let temp = prev.next;
+
+        prev.next = newNode;
+        newNode.next = temp;
+        this.length++;
+
+        return true;
+    }
+
+    remove(index) {
+        if(index < 0 || index >= this.length) return null;
+        if(index === 0) return this.shift();
+        if(index === this.length - 1) return this.pop();
+
+        let prevNode = this.get(index - 1);
+        let removed = prevNode.next;
+        prevNode.next = removed.next;
+        this.length--;
+
+        return removed;
+    }
+
     //traverse () {
     //    let current = this.head;
     //    
