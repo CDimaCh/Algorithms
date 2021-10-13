cdb4b0b62c144703869dde62fd13850ae7d9427c
Dmytro Chornyi <dkdmcho@coloplast.com>
2021-10-13 17:29:50 +0300

    added reverse

--- SinglyLinkedList/singlyLinkedList.js
+++ SinglyLinkedList/singlyLinkedList.js
@@ -127,6 +127,33 @@ class SinglyLinkedList {
         return removed;
     }
 
+    reverse () {
+        let node  = this.head;
+        this.head = this.tail;
+        this.tail = node;
+        let next;
+        let prev = null;
+
+        for (let i = 0; i < this.length; i++) {
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;  
+        }
+        return this;
+    }
+
+    print() {
+        let arr = [];
+        let current = this.head;
+
+        while(current) {
+            arr.push(current.val)
+            current = current.next;
+        }
+        console.log(arr)
+    }
+
     //traverse () {
     //    let current = this.head;
     //    
