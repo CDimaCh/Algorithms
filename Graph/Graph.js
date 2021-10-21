ec2f0a378ee9b61cb3788261b2e70589b9f70e1d
Dmytro Chornyi <dkdmcho@coloplast.com>
2021-10-21 16:09:35 +0300

    Graphs are awesome

--- Graph/Graph.js
+++ Graph/Graph.js
@@ -30,7 +30,84 @@ class Graph {
         
         delete this.adjacencyList[vertex];
     }
+
+    depthFirstRecursive(vertex) {
+        const result = [];
+        const visited = [];
+        const adjacencyList = this.adjacencyList;
+
+        (function dfs(vertex) {
+            if(!vertex) return null;
+
+            visited[vertex] = true;
+            visited.push(vertex);
+
+            adjacencyList[vertex].forEach(neighbor => {
+                if(!visited[neighbor]) {
+                    return dfs(neighbor);
+                }
+            });
+        })(start);
+
+        return result;
+    }
+
+    depthFirstIterative(start) {
+        let result = [];
+        let visited = {};
+        let stack = [start];
+        let currentVertex
+
+        visited[start] = true;
+        while(stack.length) {
+            currentVertex = stack.pop();
+            result.push(currentVertex);
+
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if(!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    stack.push(neighbor);
+                }
+            });
+        }
+
+        return result;
+    }
+
+    breadthFirstGraph(start) {
+        const queue = [start];
+        const result = [];
+        const visited = {};
+        visited[start] = true;
+
+        while(queue.length) {
+            currentVertex = queue.shift();
+            result.push(currentVertex);
+
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if(!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.push(neighbor)
+                }
+            });
+        }
+
+        return result;
+    }
 }
 
 let g = new Graph();
-g.adddVertex('Kharkiv')
+g.adddVertex('A')
+g.adddVertex('B')
+g.adddVertex('C')
+g.adddVertex('D')
+g.adddVertex('E')
+g.adddVertex('F')
+
+g.addEdge('A', 'B')
+g.addEdge('A', 'C')
+g.addEdge('B', 'D')
+g.addEdge('C', 'E')
+g.addEdge('D', 'E')
+g.addEdge('D', 'F')
+g.addEdge('E', 'F')
